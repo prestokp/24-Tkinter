d5dd453a13e4fdc580aedbf8cf440d589fbccdea
prestokp <prestokp@rose-hulman.edu>
2019-01-31 10:53:23 -0500

    Going after an A++ grade

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -3,8 +3,8 @@ This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Kirk Preston.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
@@ -16,22 +16,34 @@ def main():
     # TODO: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # -------------------------------------------------------------------------
+    window = tkinter.Tk()
+
 
     # -------------------------------------------------------------------------
     # TODO: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # -------------------------------------------------------------------------
+    frame0 = ttk.Frame(window, padding=20)
+    frame0.grid()
+    frame1 = ttk.Frame(window, padding=20)
+    frame1.grid()
+    frame2 = ttk.Frame(window, padding=20)
+    frame2.grid()
+
 
     # -------------------------------------------------------------------------
     # TODO: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # -------------------------------------------------------------------------
+    hellobutton = ttk.Button(frame0, text='hello')
+    hellobutton.grid()
 
     # -------------------------------------------------------------------------
     # TODO: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # -------------------------------------------------------------------------
+    hellobutton['command']=lambda: print('hello')
 
     # -------------------------------------------------------------------------
     # TODO: 6. After reading and understanding the m4e module,
@@ -41,6 +53,17 @@ def main():
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # -------------------------------------------------------------------------
+
+    entry_box = ttk.Entry(frame1)
+    entry_box.grid()
+    entry_button = ttk.Button(frame1, text='enter text')
+    entry_button['command'] = (lambda:
+                               print_contents(entry_box))
+    entry_button.grid()
+
+    def print_contents(entry_box):
+        entry_box_contents = entry_box.get()
+        print(entry_box_contents)
 
     # -------------------------------------------------------------------------
     # TODO: 7.
@@ -57,6 +80,11 @@ def main():
     #    that is a "user error" -- do NOT deal with that.
     #
     # -------------------------------------------------------------------------
+    multiple_entry_box = ttk.Entry(frame2)
+    multiple_entry_box.grid()
+    multiple_entry_button = ttk.Button(frame2, text='enter number')
+    multiple_entry_button['command']=(lambda:
+                                      print_contents(entry_box)
     ####################################################################
     # HINT:
     #   You will need to obtain the INTEGER from the STRING
@@ -69,6 +97,10 @@ def main():
     # -------------------------------------------------------------------------
     # TODO: 8. As time permits, do other interesting GUI things!
     # -------------------------------------------------------------------------
+
+
+
+    window.mainloop()
 
 
 # -----------------------------------------------------------------------------
