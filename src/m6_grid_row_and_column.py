af69310d97ac22fa73f3b871fa88e269d55e25e7
prestokp <prestokp@rose-hulman.edu>
2019-02-03 20:10:58 -0500

    Going after an A++ grade

--- src/m6_grid_row_and_column.py
+++ src/m6_grid_row_and_column.py
@@ -24,59 +24,59 @@ def main():
     main_frame.grid()  # only grid call that does NOT need a row and column
 
     left_speed_label = ttk.Label(main_frame, text="Left")
-    left_speed_label.grid()
+    left_speed_label.grid(column=0, row=0)
     left_speed_entry = ttk.Entry(main_frame, width=8)
     left_speed_entry.insert(0, "600")
-    left_speed_entry.grid()
+    left_speed_entry.grid(column=0, row=1)
 
     right_speed_label = ttk.Label(main_frame, text="Right")
-    right_speed_label.grid()
+    right_speed_label.grid(column=2, row=0)
     right_speed_entry = ttk.Entry(main_frame, width=8, justify=tkinter.RIGHT)
     right_speed_entry.insert(0, "600")
-    right_speed_entry.grid()
+    right_speed_entry.grid(column=2, row=1)
 
     forward_button = ttk.Button(main_frame, text="Forward")
-    forward_button.grid()
+    forward_button.grid(column=1, row=2)
     forward_button['command'] = lambda: print("Forward button")
     root.bind('<Up>', lambda event: print("Forward key"))
 
     left_button = ttk.Button(main_frame, text="Left")
-    left_button.grid()
+    left_button.grid(column=0, row=3)
     left_button['command'] = lambda: print("Left button")
     root.bind('<Left>', lambda event: print("Left key"))
 
     stop_button = ttk.Button(main_frame, text="Stop")
-    stop_button.grid()
+    stop_button.grid(column=1, row=3)
     stop_button['command'] = lambda: print("Stop button")
     root.bind('<space>', lambda event: print("Stop key"))
 
     right_button = ttk.Button(main_frame, text="Right")
-    right_button.grid()
+    right_button.grid(column=2, row=3)
     right_button['command'] = lambda: print("Right button")
     root.bind('<Right>', lambda event: print("Right key"))
 
     back_button = ttk.Button(main_frame, text="Back")
-    back_button.grid()
+    back_button.grid(column=1, row=4)
     back_button['command'] = lambda: print("Back button")
     root.bind('<Down>', lambda event: print("Back key"))
 
     up_button = ttk.Button(main_frame, text="Up")
-    up_button.grid()
+    up_button.grid(column=0, row=5)
     up_button['command'] = lambda: print("Up button")
     root.bind('<u>', lambda event: print("Up key"))
 
     down_button = ttk.Button(main_frame, text="Down")
-    down_button.grid()
+    down_button.grid(column=0, row=6)
     down_button['command'] = lambda: print("Down button")
     root.bind('<j>', lambda event: print("Down key"))
 
     # Buttons for quit and exit
     q_button = ttk.Button(main_frame, text="Quit")
-    q_button.grid()
+    q_button.grid(column=2, row=5)
     q_button['command'] = lambda: print("Quit button")
 
     e_button = ttk.Button(main_frame, text="Exit")
-    e_button.grid()
+    e_button.grid(column=2, row=6)
     e_button['command'] = lambda: exit()
 
     root.mainloop()
